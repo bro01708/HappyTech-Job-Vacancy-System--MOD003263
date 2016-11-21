521677f6ca1d56991d813e26810940bf06d1536d
Peter Brown <bro01708@gmail.com>
2016-11-21 15:27:54 +0000

    Renamed NameSpace

--- HappyTechSystem/HappyTechSystem/Program.cs
+++ HappyTechSystem/HappyTechSystem/Program.cs
@@ -17,6 +17,7 @@ namespace HappyTechSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
--- HappyTechSystem/HappyTechSystem/Question.cs
+++ HappyTechSystem/HappyTechSystem/Question.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Prototyping
+namespace HappyTechSystem
 {
     public class Question
     {       
