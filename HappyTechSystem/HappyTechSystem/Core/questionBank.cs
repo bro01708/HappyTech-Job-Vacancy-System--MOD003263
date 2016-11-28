9c26fa00516d45035ff426655a636ef0f1ea6f2e
Peter Brown <bro01708@gmail.com>
2016-11-28 14:59:28 +0000

    Fixed the test

--- HappyTechSystem/HappyTechSystem/Core/questionBank.cs
+++ HappyTechSystem/HappyTechSystem/Core/questionBank.cs
@@ -67,6 +67,7 @@ namespace HappyTechSystem.Core
                 if (q.GetID == m_question_ID)
                 {
                     questionList.Remove(q);
+                    break;
                 }
             }
         }
