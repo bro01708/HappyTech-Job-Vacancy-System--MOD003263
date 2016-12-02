56fc6af2da4358ffa575ad3457b9385f2fa84e47
Suzie <Suzie@LEMMAB-HP>
2016-12-02 13:19:06 +0000

    Started some unit testing

---
+++ HappyTechSystem/UnitTests/EmailBankTests.cs
@@ -0,0 +1,33 @@
+﻿using HappyTechSystem.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+
+    public class EmailBankTests
+    {
+        /// <summary>
+        /// Created by Susan
+        /// Tests adding a template to the email bank
+        /// 02/12/2016
+
+        [TestMethod]
+
+        public void Test_addTemplateToList()
+        {
+            EmailBank EB = new EmailBank();
+            EmailTemplate T = new EmailTemplate();
+            int expected = EB.getTemplateList.Count;
+            EB.addTemplateToList(T);
+
+            Assert.AreEqual(EB.getTemplateList[expected], T);
+
+        }
+
+
+        }
+
+    
+}
